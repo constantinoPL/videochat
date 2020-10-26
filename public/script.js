d779a4206829ce2087e328746ef9c9e090dac270
const <plahetko.constantin@gmail.com>
2020-10-26 23:13:15 +0100

    play stop mute unmute buttons

--- public/script.js
+++ public/script.js
@@ -76,3 +76,57 @@ const scrollBottom = () => {
   let d = $('.main__chat_window');
   d.scrollTop(d.prop('scrollHeight'));
 };
+
+const muteUnmute = () => {
+  const { enabled } = myVideoStream.getAudioTracks()[0];
+  if (enabled) {
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    setUnmuteButton();
+  } else {
+    setMuteButton();
+    myVideoStream.getAudioTracks()[0].enabled = true;
+  }
+};
+
+const setMuteButton = () => {
+  const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>  
+  `;
+  document.querySelector('.main__mute_button').innerHTML = html;
+};
+
+const setUnmuteButton = () => {
+  const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>  
+  `;
+  document.querySelector('.main__mute_button').innerHTML = html;
+};
+
+const playStop = () => {
+  const { enabled } = myVideoStream.getVideoTracks()[0];
+  if (enabled) {
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    setPlayVideo();
+  } else {
+    setStopVideo();
+    myVideoStream.getVideoTracks()[0].enabled = true;
+  }
+};
+
+const setStopVideo = () => {
+  const html = `
+   <i class="fas fa-video"></i>
+   <span>Stop Video</span>
+   `;
+  document.querySelector('.main__video_button').innerHTML = html;
+};
+
+const setPlayVideo = () => {
+  const html = `
+   <i class="fas fa-video"></i>
+   <span>Play Video</span>
+   `;
+  document.querySelector('.main__video_button').innerHTML = html;
+};
